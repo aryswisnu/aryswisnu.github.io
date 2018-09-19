2eae83db815633a52194a90e25946a0185d6a88d
I Kadek Bayu Arys Wisnu Kencana <bayuarys2010@gmail.com>
2018-09-19 21:31:00 +0700

    remove projects dir

--- projects/companyprofile-mockup/README.md
+++
@@ -1,3 +0,0 @@
-# Company Profile Mockup
-
-To view it online, please kindly visit this url [aryswisnu.github.io/projects/companyprofile-mockup/](https://aryswisnu.github.io/projects/companyprofile-mockup/)
--- projects/gatotkacashooter/connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$host = "ftp.hebat.web.id";
-	$user = "hebatweb_audit";
-	$password = "Audit333234";
-	$conn = mysql_connect($host,$user,$password);
-	if($conn){
-		$mydb = mysql_select_db('hebatweb_virtual_fittingroom',$conn);
-	}
-?>
